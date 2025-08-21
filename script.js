c4758581761271f0447fc73573fb45656f2801b2
Estrela15 <145451959+Estrela15@users.noreply.github.com>
2025-08-21 08:32:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,38 +6,108 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "A harpia (também conhecida como gavião-real) é uma das aves de rapina mais impressionantes do mundo. Sua força e tamanho a tornam uma predadora de topo de cadeia, e há muitas curiosidades sobre ela.",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "isso é chato",
+                afirmacao: "Parece desinterresante. "
+            },
+            {
+                texto: "Que interressante!",
+                afirmacao: "Quis saber sobre a harpia."
+            }
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "A perda de seu habitat natural é a maior ameaça. As harpias precisam de vastas áreas de florestas contínuas para caçar e se reproduzir. Com o desmatamento, as florestas se fragmentam, e a ave perde não só seu lar, mas também a sua fonte de alimento, como preguiças e macacos.?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Denuncie crimes ambientais: Se presenciar caça ilegal ou desmatamento, avise os órgãos competentes, como o Ibama.",
+                afirmacao: "Quero ajudar com a causa."
+            },
+            {
+                texto: "Acabar de vez com a especie, não é problema meu.",
+                afirmacao: "Não me interresso por esse assunto e não me importo com o que vai acontecer com a espécie."
+            }
         ]
     },
     {
-        enunciado: "Pergunta 3",
+        enunciado: "Símbolo nacional: A harpia é a ave nacional do Panamá e estampa o brasão de armas do país.",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Por esse motivo seria bom conservar a especie e protege-la de ameaças de estinção, pois ela representa nosso país.",
+                afirmacao: "acho muito importante proteger a espécie, é uma grande representante do país."
+            },
+            {
+                texto: "É antigo, tem que mudar mesmo. Me importo mais com outras espécies",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
         ]
     },
     {
-        enunciado: "Pergunta 4",
+        enunciado: "A principal função da harpia é atuar como um controlador natural de outras espécies. Sua dieta é composta por animais que vivem nas árvores, como preguiças, macacos e quatis. Ao caçar esses animais, a harpia ajuda a manter suas populações sob controle, o que evita que eles se multipliquem em excesso e causem danos à vegetação.",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "É uma espécie muito importante, deve ser valorizada e conservada a todo custo, não podemos perder esta ave tão importante e representativa para o país.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Temos outras aves que são mais importantes, uma a menos não vai fazer falta.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
         ]
     },
     {
-        enunciado: "Pergunta 5",
+        enunciado: "Existem vários projetos e iniciativas dedicados à proteção da harpia. O Projeto Harpia, por exemplo, trabalha na pesquisa e monitoramento da espécie em diversas regiões do Brasil. Eles colaboram com comunidades locais para educar sobre a importância da ave e também promovem ações de manejo e reintrodução em cativeiro para tentar restaurar as populações. O trabalho de conservação inclui também a reabilitação de aves feridas e a criação de programas para proteger ninhos. A conservação da harpia é fundamental para manter o equilíbrio de ecossistemas florestais, e o sucesso desses esforços depende da conscientização e do apoio de toda a sociedade. ",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
         ]
-    }
+    },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
